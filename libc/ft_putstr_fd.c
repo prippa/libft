a0120bc0b52c71ecf76df3162931510ea2c01ffe
Pavel Rippa <prippa@MacBook-Pro-Pavel.local>
2018-02-03 17:07:52 +0200

    optimize code

--- libc/ft_putstr_fd.c
+++ libc/ft_putstr_fd.c
@@ -14,15 +14,6 @@
 
 void	ft_putstr_fd(char const *s, int fd)
 {
-	size_t i;
-
 	if (s)
-	{
-		i = 0;
-		while (s[i])
-		{
-			ft_putchar_fd(s[i], fd);
-			i++;
-		}
-	}
+		write(fd, s, ft_strlen(s));
 }
