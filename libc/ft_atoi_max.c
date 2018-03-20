7d772bc675a3660faaebf0a3efdb62c83e0fd62d
prippa <pavelrippa@gmail.com>
2018-03-20 14:09:37 +0200

    Updating after push_swap

---
+++ libc/ft_atoi_max.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_max.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prippa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/05 13:02:54 by prippa            #+#    #+#             */
+/*   Updated: 2018/03/05 13:03:01 by prippa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long long int	ft_atoi_max(char *str)
+{
+	int				i;
+	long long int	res;
+	int				sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
